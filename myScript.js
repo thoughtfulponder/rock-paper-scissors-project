9b27ae6ae272ecca0a76db77ef9d4b01bc94d43c
thoughtfulponder <61815746+thoughtfulponder@users.noreply.github.com>
2025-09-25 18:18:10 +0000

    working on playRound function

--- myScript.js
+++ myScript.js
@@ -9,41 +9,48 @@ function getComputerChoice() {
   let computerAnwser;
   if (randomNumber === 1) {
     computerAnwser = "Rock";
-    console.log("Rock");
+    // console.log("Rock");
   } else if (randomNumber === 2) {
     computerAnwser = "Paper";
-    console.log("Paper");
+    // console.log("Paper");
   } else {
     computerAnwser = "Scissors";
-    console.log("Scissors");
+    // console.log("Scissors");
   }
   return `computer choosed ${computerAnwser}!`;
 }
 console.log(getComputerChoice());
 
-function humanChoice() {
+function getHumanChoice() {
   
   let capChara = userChoice.slice(0, 1).toUpperCase();
   let lowerChara = userChoice.slice(1, userChoice.length).toLowerCase();
   userChoice = capChara + lowerChara;
   return `human choosed ${userChoice}!`;
 }
-console.log(humanChoice());
+console.log(getHumanChoice());
 
-function playRound(computerChoice, getHumanChoice) {
-  if (getComputerChoice === "Rock" && humanChoice === "Paper" || getComputerChoice === "Paper" && humanChoice === "Scissors" || getComputerChoice === "Scissors" && humanChoice === "Rock") {
+function playRound(computerChoice, humanChoice) {
+  if (computerChoice === "Rock" && humanChoice === "Paper" 
+    || computerChoice === "Paper" && humanChoice === "Scissors" 
+    || computerChoice === "Scissors" && humanChoice === "Rock") {
     console.log("Human win this round!");
     humanScore++;
     console.log(`human scored ${humanScore}`);
-  } else if (getComputerChoice === "Rock" && humanChoice === "Scissors" || getComputerChoice === "Paper" && humanChoice === "Rock" || getComputerChoice === "Scissors" && humanChoice === "Paper") {
+  } else if (computerChoice === "Rock" && humanChoice === "Scissors" 
+    || computerChoice === "Paper" && humanChoice === "Rock" 
+    || computerChoice === "Scissors" && humanChoice === "Paper") {
     console.log("computer win this round!");
     computerScore++;
     console.log(`computer scored ${computerScore}`);
   } else {
     console.log("draw");
   }
+ 
 }
 
-const humanSelection = humanChoice();
+const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-playRound(computerSelection, humanSelection);
+
+
+console.log(playRound(computerSelection, humanSelection));
